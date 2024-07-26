b8668222337132e693f830c032d2806359932ad9
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-07-26 13:49:10 +0530

    done the user routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model";
 const bcrypt = require ("bcrypt");
+const jwt = require ('jsonwebtoken');
 
 
 //register
@@ -43,3 +44,134 @@ export const register = async (req,res)=>{
 }
 
 //for login 
+
+export const login = async (req,res) =>{
+    try {
+        const { email,password,role}=req.body;
+        if(!email || !password ||!role){
+        return res.status(400).json({
+            message: "something is missing",
+            success: false,
+        })
+    };
+      let user = await User.findOne({email});
+       if (!user) {
+        return res.status(400).json({
+            message:"Incorrect Email or password",
+            success: false,
+        })
+       }
+       const passwordmatch = await bcrypt.compare(password, user.password);//compare the password
+       if (!passwordmatch) {
+        return res.status(400).json({
+            message:"Incorrect Email or password",
+            success: false,
+        })
+        
+       };
+       //check the role (student or recruiter)
+if (role !== user.role) {
+    return res.status(400).json({
+        message:"Account doesn't exist with current role. ",
+        success: false,
+    })
+}
+//token
+
+const tokenData ={
+    userId:user._id
+}
+const token = await jwt.sign (tokenData, process.env. SECRET_KEY,{expires :'1d'});
+
+
+user = {
+    _id: user._id,
+    fullname: user.fullname,
+    email: user.email,
+    phonenumber: user.phonenumber,
+    role: user.role,
+    profile: user.profile
+}
+
+//store the token in cookies
+ return res.status(200).cookie("token", token,{ maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict'}).json({
+    message:`Welcome back ${user.fullname}`,
+    success: true,
+ })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//logout
+
+export const logout = async (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logged out successfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//update profile
+
+export const updateProfile = async(req,res)=>{
+    try {
+        const { fullname, email, phonenumber, bio, skills } = req.body;
+
+      const  file = req.file;
+
+        if(!fullname || !email || !phonenumber || !bio || !skills ){
+            return res.status(400).json ({
+                message:"something is missing",
+                success:false
+            });
+        };
+
+        //cloudinary will be added
+
+        const skillsArray = skills.split(",")
+        const userId = req.id//middleware
+        let user = await User.findById(userId);
+        if (!user) {
+            return res.status(400).json({
+                message:"User not found",
+                success:false,
+            })
+            
+        }
+
+        //updating data
+
+        user.fullname = fullname,
+        user.email = email,
+        user.phonenumber = phonenumber,
+        user.profile.bio = bio
+        user.profile.skills= skillsArray
+
+        //resume will added
+        
+    await user.save();
+
+    user = {
+        _id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+        phonenumber: user.phonenumber,
+        role: user.role,
+        profile: user.profile
+    }
+    return res.status(200).json({
+        message:"Profile updated successfully.",
+        user,
+        success:true
+    })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
