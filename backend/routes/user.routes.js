c6e5107d09b39c086f8d6a33418820ec72c4ac76
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-07-30 18:51:57 +0530

    installed the multer for file uploads

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import authenticated from "../middleware/authenticated.js"
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/profile/update").post(authenticated,updateProfile);
 router.route("/logout").get(logout);
