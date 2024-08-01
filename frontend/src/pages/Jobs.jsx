103cca889c1d684c685b093e3fe51784285559a6
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-08-01 17:36:19 +0530

    done the job cards

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,18 +1,39 @@
-import Navbar from '@/components/shared/Navbar'
-import React from 'react'
-import FilterCards from './FilterCards'
+import Navbar from "@/components/shared/Navbar";
+import React from "react";
+import FilterCards from "./FilterCards";
+import Jobsss from "./Jobsss";
 
+const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const jobsArray=[1,2,3,4,5,6,7,8];
 const Jobs = () => {
   return (
     <div>
-        <Navbar/>
-        <FilterCards/>
-       
-     
-    </div>
-  )
-}
+      <Navbar />
 
-export default Jobs
+      <div className="max-w-7xl mx-auto mt-5">
+        <div className="flex gap-5">
+          <div className="w-20%">
+            <FilterCards />
+          </div>
+          {
+            jobsArray.length <= 0 ? (
+              <span>Job not Found</span>
+            ) : (
+              <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
+                <div className="grid grid-cols-3 gap-4">
+                  {jobsArray.map((item, index) => (
+                    <div key={index}>
+                      <Jobsss />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jobs;
---
+++ frontend/src/pages/Jobsss.jsx
@@ -0,0 +1,46 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Bookmark } from "lucide-react";
+import React from "react";
+
+const Jobsss = () => {
+  return (
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
+      <div className="flex ityems-center justify-between">
+        <p className="text-sm text-gray-500">2 days ago</p>
+        <Button variant="outline" className="rounded-full" size="icon">
+          <Bookmark />
+        </Button>
+      </div>
+
+      <div className="flex items-center gap-2 my-2">
+        <Button className="p-6" variant="outline" size="icon">
+          <Avatar>
+            <AvatarImage src="https://i.pinimg.com/originals/09/2f/7b/092f7b121aaabf4449aee3555b0f26a2.png" />
+          </Avatar>
+        </Button>
+        <div>
+          <h1 className="font-medium text-lg">company Name</h1>
+          <p className="text-sm text-gray-500">India</p>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold text-lg my-2">Title</h1>
+        <p className="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nesciunt ex earum culpa quod, aut pariatur excepturi non dignissimos eaque distinctio doloribus delectus facilis recusandae adipisci quasi, fuga quos impedit?</p>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+       <Badge className={"text-blue-700 font-bold"} variant="ghost"> 12 positions</Badge>
+        <Badge className={"text-[#821536] font-bold"} variant="ghost"> Part Time</Badge>
+        <Badge className={"text-[#621a9e] font-bold"} variant="ghost"> 24LPA</Badge>
+        </div>
+        <div className="flex items-center gap-4 mt-4">
+        <Button variant="outline"> Details</Button>
+        <Button className="bg-[#621a9e] font-bold">Save For Later</Button>
+        </div>
+       
+    </div>
+  );
+};
+
+export default Jobsss;
