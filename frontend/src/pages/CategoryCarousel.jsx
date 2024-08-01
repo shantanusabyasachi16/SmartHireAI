f1937b5992aac1ab5a0e8cdecda54529f731bc63
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-08-01 10:34:36 +0530

    done the  category section

--- frontend/src/pages/CategoryCarousel.jsx
+++ frontend/src/pages/CategoryCarousel.jsx
@@ -1,8 +1,36 @@
+import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import React from 'react'
-
+const category= [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+   "FullStack Development",
+   "Cyber Security",
+   "DevOps"
+]
 const CategoryCarousel = () => {
   return (
-    <div>CategoryCarousel</div>
+    <div>
+      <Carousel  className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {
+            category.map((cat,index ) =>(
+              <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+           <Button variant="outline" className="rounded-full font-bold text-[#6f597a]">{cat}</Button>
+              </CarouselItem>
+
+              )  )
+
+        
+          }
+        
+        </CarouselContent>
+        <CarouselPrevious/>
+        <CarouselNext/>
+      </Carousel>
+    </div>
   )
 }
 
