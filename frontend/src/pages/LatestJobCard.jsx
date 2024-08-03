6e5a767b9cced14c6127c9f7c137e41996807ba7
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-08-03 19:05:00 +0530

    done the functionality in jobs section

--- frontend/src/pages/LatestJobCard.jsx
+++ frontend/src/pages/LatestJobCard.jsx
@@ -1,21 +1,21 @@
 import { Badge } from '@/components/ui/badge'
 import React from 'react'
 
-const LatestJobCard = () => {
+const LatestJobCard = ({job}) => {
   return (
     <div className='p-5 rounded-md shadow-xl bg-white border-gray-100 cursor-pointer'>
         <div>
-        <h1 className='font-medium text-lg'>Company Name</h1>
+        <h1 className='font-bold text-bold text-lg'>{job?.company?.name}</h1>
         <p className='text-sm text-blue-600'>India</p>
         </div>
       <div>
-        <h1 className='font-bold text-lg my-2'>Job Title</h1>
-        <p className=' text-sm  text-black-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nihil aliquam soluta illo vel.</p>
+        <h1 className='font-medium text-lg my-2'>{job?.title}</h1>
+        <p className=' text-sm  text-black-200'>{job?.description}</p>
       </div>
       <div className=' flex items-center gap-2 mt-4'>
-        <Badge className={"text-blue-700 font-bold"} variant="ghost"> 12 positions</Badge>
-        <Badge className={"text-[#821536] font-bold"} variant="ghost"> Part Time</Badge>
-        <Badge className={"text-[#621a9e] font-bold"} variant="ghost"> 24LPA</Badge>
+        <Badge className={"text-blue-700 font-bold"} variant="ghost"> {job?.position}</Badge>
+        <Badge className={"text-[#821536] font-bold"} variant="ghost"> {job?.jobType}</Badge>
+        <Badge className={"text-[#621a9e] font-bold"} variant="ghost">{job?.salary}</Badge>
       </div>
     </div>
   )
--- frontend/src/pages/Latestjobs.jsx
+++ frontend/src/pages/Latestjobs.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import LatestJobCard from './LatestJobCard'
-const random =[1,2,3,4,5,6,7,8,9,10 ,11,12]
+import React from 'react';
+import LatestJobCard from './LatestJobCard';
+import { useSelector } from 'react-redux';
+
 const Latestjobs = () => {
+  const {  allJobs } = useSelector((store) => store.job);
+
   return (
     <div className='max-w-7xl mx-auto my-25'>
-      <h1 className=' text-4xl font-bold'><span className='text-[#591887]'>Latest & Top</span> JOB OPENINGS</h1>
-   <div className='grid grid-cols-3 gap-4 my-5'>
-   {
-      random.slice(0,6).map((item,index)=><LatestJobCard/>)
-    }
-   </div>
-   
+      <h1 className='text-4xl font-bold'>
+        <span className='text-[#591887]'>Latest & Top</span> JOB OPENINGS
+      </h1>
+      <div className='grid grid-cols-3 gap-4 my-5'>
+        { allJobs.length === 0 ? (
+          <span>No job available</span>
+        ) : (
+          allJobs.slice(0, 6).map((job) => <LatestJobCard key={job._id} job={job} />)
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Latestjobs
+export default Latestjobs;
