e98d1d230f96d36826af179c5fae704bdb9e1ecb
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-08-04 19:19:40 +0530

    installed redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
 import jobSlice from './jobSlice';
 
+
 const store = configureStore({
   reducer: {
     auth: authSlice,
